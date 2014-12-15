65edc81ebcae5dddf1243ac67941ca8757b682bc
jonsag <jonsagebrand@gmail.com>
2014-12-15 15:37:27 +0100

    Added current jsPowerTempLog

---
+++ RaspberryPi/jsPowerTempLog/install/add-1wire-devices.sql
@@ -0,0 +1,9 @@
+INSERT INTO 1wireDevices 
+(devicePath, deviceType, place) 
+VALUES 
+('28-000003c37731', 'temp', 'ute');
+
+INSERT INTO 1wireDevices 
+(devicePath, deviceType, place) 
+VALUES 
+('28-000003c359ac', 'temp', 'boden');
